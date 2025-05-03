43326dff718c4481181f2cd397ec87c04b6a32de
bashir <mohammadbashir266@gmail.com>
2025-05-03 14:49:36 +0300

    Add keyboard input handling

--- desktop/src/main.rs
+++ desktop/src/main.rs
@@ -7,6 +7,7 @@ use sdl2::pixels::Color;
 use sdl2::rect::Rect;
 use sdl2::render::Canvas;
 use sdl2::video::Window;
+use sdl2::keyboard::Keycode;
 
 const SCALE: u32 = 15;
 const WINDOW_WIDTH: u32 = (SCREEN_WIDTH as u32)*SCALE;
@@ -45,8 +46,16 @@ fn main() {
     'gameloop: loop {
         for evt in event_pump.poll_iter() {
             match evt {
-                Event::Quit { .. } => {
-                    break 'gameloop;
+                Event::Quit { .. } => break 'gameloop,
+                Event::KeyDown { keycode: Some(key), .. } => {
+                    if let Some(k) = key2btn(key) {
+                        emu.keypress(k, true);
+                    }
+                },
+                Event::KeyUp { keycode: Some(key), .. } => {
+                    if let Some(k) = key2btn(key) {
+                        emu.keypress(k, false);
+                    }
                 },
                 _ => ()
             }
@@ -79,4 +88,26 @@ fn draw_screen(canvas: &mut Canvas<Window>, emu: &Emu) {
         }
     }
     canvas.present();
+}
+
+fn key2btn(key: Keycode) -> Option<usize> {
+    match key {
+        Keycode::Num1 => Some(0x1),
+        Keycode::Num2 => Some(0x2),
+        Keycode::Num3 => Some(0x3),
+        Keycode::Num4 => Some(0xC),
+        Keycode::Q => Some(0x4),
+        Keycode::W => Some(0x5),
+        Keycode::E => Some(0x6),
+        Keycode::R => Some(0xD),
+        Keycode::A => Some(0x7),
+        Keycode::S => Some(0x8),
+        Keycode::D => Some(0x9),
+        Keycode::F => Some(0xE),
+        Keycode::Z => Some(0xA),
+        Keycode::X => Some(0x0),
+        Keycode::C => Some(0xB),
+        Keycode::V => Some(0xF),
+        _ => None,
+    }
 }
